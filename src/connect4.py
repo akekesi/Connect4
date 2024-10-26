f488e995804c3d9261b0a36844d7449bc83dfdff
akekesi <kekesi.att@gmail.com>
2024-10-26 16:27:59 +0200

    add print_pritty()

--- src/connect4.py
+++ src/connect4.py
@@ -1,3 +1,8 @@
+# TODO: agent random
+# TODO: agent minimax / alpha-beta
+# TODO: game with info (new game, 1/2player, difficulty,... )
+# TODO: mark the winning discs
+
 """
 Connect4 Game Module
 
@@ -26,7 +31,9 @@ from src.check_finish import check_finish
 # set logger up
 logger = Logging().set_logger(
     name="Connect4",
-    level=logging.DEBUG,
+    # level=logging.NOTSET,
+    level=logging.INFO,
+    # level=logging.DEBUG,
     path_dir=os.path.join(os.path.dirname(__file__), "..", "logs")
 )
 
@@ -46,6 +53,11 @@ class Connect4:
         self.col = 7
         self.win = 4
         self.discs = [1, 2]
+        self.tiles = {
+            0: " ",
+            1: "x",
+            2: "o",
+        }
         self.board = np.zeros((self.row, self.col))
         logger.info("board is initialized")
         logger.debug("1")
@@ -87,22 +99,6 @@ class Connect4:
         logger.debug("1")
         return row
 
-    def check_valid_col(self, col: int) -> bool:
-        """
-        Checks if the given column index is valid (within board bounds).
-
-        Args:
-            col (int): The column index to check.
-
-        Returns:
-            bool: True if the column index is valid, False otherwise.
-        """
-        logger.debug("0")
-        valid = 0 <= col < self.col
-        logger.info("%s", valid)
-        logger.debug("1")
-        return valid
-
     def check_valid_row(self, row: int) -> bool:
         """
         Checks if the given row index is valid (within board bounds).
@@ -115,6 +111,22 @@ class Connect4:
         """
         logger.debug("0")
         valid = 0 <= row < self.row
+        logger.info("%s", valid)
+        logger.debug("1")
+        return valid
+
+    def check_valid_col(self, col: int) -> bool:
+        """
+        Checks if the given column index is valid (within board bounds).
+
+        Args:
+            col (int): The column index to check.
+
+        Returns:
+            bool: True if the column index is valid, False otherwise.
+        """
+        logger.debug("0")
+        valid = 0 <= col < self.col
         logger.info("%s", valid)
         logger.debug("1")
         return valid
@@ -149,35 +161,63 @@ class Connect4:
         return True
 
     def game(self) -> None:
-        """
-        """
         # TODO: init board ???
         logger.debug("0")
+        turn = 0
+        self.print_pritty(turn=turn)
         while True:
-            disc = int(input("Enter your disc value: "))
-            col = int(input("Enter column number: "))
+            disc = self.discs[turn % 2]
+            turn += 1
+            col = int(input(f"Enter column number to set '{self.tiles[disc]}' of player-{disc}: "))
             # TODO: check input
             move_info = self.move(disc=disc, col=col)
             if not move_info:
                 logger.info("Invalid move.")
+                turn -= 1
                 continue
-            print(self.board)
-            if disc == check_finish(board=self.board, disc=disc, win=self.win):
-                logger.info("Player-%d won.", disc)
+            self.print_pritty(turn=turn)
+            win = check_finish(board=self.board, disc=disc, win=self.win)
+            if win == disc:
+                win_str = f"Player-{disc} won."
+                print(win_str)
+                logger.info(win_str)
                 break
-            if 0 == check_finish(board=self.board, disc=disc, win=self.win):
-                logger.info("The board is full, resulting in a draw.")
+            if win == 0:
+                win_str = "The board is full, resulting in a draw."
+                print(win_str)
+                logger.info(win_str)
                 break
         logger.debug("1")
+
+    def print_pritty(self, turn: int) -> None:
+        """
+        |==============|
+        |              |
+        |              |
+        |    X X       |
+        |    O X X     |
+        |  O X O O     |
+        |  O O X X     |
+        |==============|
+        |0 1 2 3 4 5 6 |
+        """
+        print("|=============|")
+        for row in range(self.row):
+            row_str = "|"
+            for col in range(self.col):
+                row_str += self.tiles[self.board[row, col]]
+                if col < self.col - 1:
+                    row_str += " "
+                else:
+                    row_str += "|"
+            print(row_str)
+        print("|=============|")
+        print("|0 1 2 3 4 5 6|")
+        print(f"|{f'Turn:{turn:02}':=^13}|")
+        print()
 
 
 if __name__ == "__main__":
     connect4 = Connect4()
-
-    connect4.move(disc=1, col=2)  # Player 1 places a disc in column 2
-    connect4.move(disc=2, col=3)  # Player 2 places a disc in column 3
-    connect4.move(disc=1, col=3)  # Player 1 places a disc in column 3
-
-    print(connect4.board)
 
     connect4.game()
